a6c353862a2bac32a8241e810f16a5e4aaed20c9
Jellona <leo.malinen@kolumbus.fi>
2017-05-28 23:55:15 +0300

    Update courseJavaScript.js

--- courseJavaScript.js
+++ courseJavaScript.js
@@ -2,14 +2,17 @@
 // Creating JSON for the courses which already exist when user logs in
 // For the images I used googles "Saa käyttää uudelleen ja muokata" search option in order to not use copyrighted photos
 
-var JSONtext = '[{"courseName":"Mobile programming","Credits":"5","Timing":"4th semester","Language":"English","Type":"Compulsory","imageName":"https://static.pexels.com/photos/270557/pexels-photo-270557.jpeg"}' 
+var JSONtext = '[{"courseName":"Mobile programming","Credits":"5","Timing":"4th semester","Language":"English","Type":"Compulsory","imageName":"https://static.pexels.com/photos/270557/pexels-photo-270557.jpeg"}'
 + ',{"courseName":"Server Technologies","Credits":"5","Timing":"3rd semester","Language":"English","Type":"Compulsory","imageName":"https://upload.wikimedia.org/wikipedia/commons/e/e0/A_view_of_the_server_room_at_The_National_Archives.jpg"}'
 + ',{"courseName":"Finnish for beginners","Credits":"3","Timing":"2nd semester","Language":"Finnish","Type":"Compulsory","imageName":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/2000px-Flag_of_Finland.svg.png"}'
 + ',{"courseName":"3D printing","Credits":"5","Timing":"none","Language":"English","Type":"Optional","imageName":"https://upload.wikimedia.org/wikipedia/commons/b/b8/Felix_3D_Printer_-_Printing_Head.JPG"}]';
 
 
-
-// ----- Showing courses on html page (homePage.html)
+//////////////////////////////////////////////////////////////////////////////
+// ----- Showing courses on html page (homePage.html) (the courses with pictures above them)
+var i;
+var a;
+var courses;
 
 function listCoursesOnPage() {
     courses = JSON.parse(JSONtext);
@@ -17,14 +20,14 @@ function listCoursesOnPage() {
     var containerElement = document.getElementById("course_container");
     containerElement.innerHTML = "";
     
-    for (var i = 0; i<courses.length; i++) {
+    for (i = 0; i < courses.length; i++) {
         
         var divElement = document.createElement("div");
         var imgElement = document.createElement("img");
         var pElement = document.createElement("p");
         
         imgElement.setAttribute("src", courses[i].imageName);
-        imgElement.setAttribute("class", "courseImageList");        
+        imgElement.setAttribute("class", "courseImageList");
         divElement.appendChild(imgElement);
         
         pElement.innerHTML = courses[i].courseName + ", Credits: " + courses[i].Credits;
@@ -32,21 +35,21 @@ function listCoursesOnPage() {
         divElement.appendChild(pElement);
         
         divElement.setAttribute("class", "courseDivList");
-        divElement.setAttribute("onclick", "showOnlyThisCourseWithBiggerImage("+i+");");
+        divElement.setAttribute("onclick", "showOnlyThisCourseWithBiggerImage(" + i + ");");
         containerElement.appendChild(divElement);
         
         
     }
 }
-
+//////////////////////////////////////////////////////////////////////////////
 // ------ Showing course image as bigger and more info on course
 
 function showOnlyThisCourseWithBiggerImage(index) {
     var containerElement = document.getElementById("course_container");
-    containerElement.innerHTML= "";
+    containerElement.innerHTML = "";
     
     
-    var divElement = document.createElement("div");
+        var divElement = document.createElement("div");
         var imgElement = document.createElement("img");
         var pElement = document.createElement("p");
         
@@ -65,43 +68,175 @@ function showOnlyThisCourseWithBiggerImage(index) {
 }
 
 
-// ------ Adding new courses
 
-function addCourseToContainer(){
+
+//////////////////////////////////////////////////////////////////////////////
+// ------ Adding new courses
+/*
+inputCName
+inputCCredits
+inputCTiming
+inputCLanguage
+inputCType
+
+cName, cCredits, cTiming, cLanguage, cType
+*/
+function Player(cName, cCredits, cTiming, cLanguage, cType) {
+    "use strict";
+    this.cName = cName;
+    this.cCredits = cCredits;
+    this.cTiming = cTiming;
+    this.cLanguage = cLanguage;
+    this.cType = cType;
+    
+    this.toString = function () {
+        var output = "Name: " + this.cName + ", Credits: " + this.cCredits + ", Timing: " + this.cTiming + ", Language: " +  this.cLanguage + ", Type: " + this.cType;
+        console.log(output);
+        return output;
+    }
+}
+
+var playerArray = [];
+
+
+function writePlayerArrayToWebStorage() {
+    // Serializing JavaScript objects into Json text/String
+    var JSONtext = JSON.stringify(playerArray);
+    
+    // Saving em as text to Web storage
+    // localStorage.setItem("playerArrayString", JSONtext);
+    sessionStorage.setItem("playerArrayString", JSONtext);
+}
+
+function readPlayerArrayFromWebStorage() {
+    // Fetching JSOntext from Web storage
+    // var JSONtext = localStorage.getItem("playerArrayString");
+    var JSONtext = sessionStorage.getItem("playerArrayString");
+    
+    // Deserializing JavaScript objects from Json text/String
+    playerArray = JSON.parse(JSONtext);
+    
+   
+}
+
+function displayPlayerListOnScreen() {
+    if (playerArray !== null && playerArray.length > 0) {
+    var parentElement = document.getElementById("playerList");
+    parentElement.innerHTML = "";
+    
+    
+        for (a=0; a < playerArray.length; a++) {
+            childElement = document.createElement("li");
+            childElement.innerHTML = playerArray[a].toString();
+            parentElement.appendChild(childElement);
+        }
+    }
+    
+}
+
+function addPlayerToList() {
     "use strict";
     // Reading input
-    var courseName = document.getElementById("inputName").value;
-    var Credits = document.getElementById("inputCredits").value;
-    var Timing = document.getElementById("inputTiming").value;
-    var Language = document.getElementById("inputLanguage").value;
-    var Type = document.getElementById("inputType").value;
+    var cName = document.getElementById("inputCName").value;
+    var cCredits = document.getElementById("inputCCredits").value;
+    var cTiming = document.getElementById("inputCTiming").value;
+    var cLanguage = document.getElementById("inputCLanguage").value;
+    var cType = document.getElementById("inputCType").value;
     
     //Adding new plyaer to collection
-    var courseTemp = new Course(courseName, Credits, Timing, Language, Type);
-    courseArray.push(courseTemp);
+    var playerTemp = new Player(cName, cCredits, cTiming, cLanguage, cType);
+    playerArray.push(playerTemp);
     
     //Writing the array also the the web(browser) storage
-    writeCourseArrayToWebStorage();
+    writePlayerArrayToWebStorage();
     
     //Clearing input
-    document.getElementById("inputName").value = "";
-    document.getElementById("inputPoints").value = "";
+    document.getElementById("inputCName").value = "";
+    document.getElementById("inputCCredits").value = "";
+    document.getElementById("inputCTiming").value = "";
+    document.getElementById("inputCLanguage").value = "";
+    document.getElementById("inputCType").value = "";
     
     //Displaying also the the web(browser) storage
-    displayCourseListOnScreen();
+    displayPlayerListOnScreen();
 }
 
 
 
-// ------ Getting list of current selected courses
 
 
+////////////////////////////////////////////////////////////////////////////////////
+// ------ Geolocation functions
+var x = document.getElementById("location");
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation not support with your browser.";
+    }
+}
 
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
 
+//////////////////////////////////////////////////////////////////////////////
+// ------ Sidebar navigator functions
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// ------ Adding courses function
+
+function addCourseToListSelected(){
+    var ul = document.getElementById("selected_list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Course"));
+    ul.appendChild(li);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// ------- Angular function for the goals, removing them and adding new ones
+
+angular.module('goalApp', [])
+  .controller('GoalListController', function() {
+    var goalList = this;
+    goalList.goals = [
+      {text:'Choose compulosry courses', done:false},
+      {text:'Choose optional courses', done:false},
+      {text:'Choose 60 credits worth of courses', done:false}];
+
+    goalList.addGoal = function() {
+      goalList.goals.push({text:goalList.goalText, done:false});
+      goalList.goalText = '';
+    };
+
+    goalList.remaining = function() {
+      var count = 0;
+      angular.forEach(goalList.goals, function(goal) {
+        count += goal.done ? 0 : 1;
+      });
+      return count;
+    };
+
+    goalList.archive = function() {
+      var oldGoals = goalList.goals;
+      goalList.goals = [];
+      angular.forEach(oldGoals, function(goal) {
+        if (!goal.done) goalList.goals.push(goal);
+      });
+    };
+  });
 
 
 // ----------- Executing the JavaScript functions
+// ------- Running functions
+
 listCoursesOnPage();
